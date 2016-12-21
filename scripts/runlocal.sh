9988d7cc702c3e4af4c86c13dd408661937a16bd
Emlyn O'Regan <emlynoregan@gmail.com>
2016-12-21 17:02:06 +1030

    auto

--- scripts/runlocal.sh
+++ scripts/runlocal.sh
@@ -13,7 +13,7 @@ git pull origin master  || { echo '011' ; exit 1; }
 #2: Run generate and run tests
 cd "$1" || { echo '020' ; exit 1; }
 /usr/bin/haxe buildtests.hxml || { echo '030' ; exit 1; }
-python "$1/fixpy.py" < "$1/sUTLHaxePython/sUTLTestsPre.py" > "$1/sUTLHaxePython/sUTLTests.py"
+python "$1/fixpy.py" < "$1/sUTLHaxePython/sUTLTestsPre.py" > "$1/sUTLHaxePython/sUTLTests.py" || { echo '035' ; exit 1; }
 python "$1/sUTLHaxePython/sUTLTests.py" || { echo '040' ; exit 1; }
 
 #3: Generate the python interpreter
